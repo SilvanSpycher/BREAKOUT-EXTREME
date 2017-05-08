0cd933da3757310f2bbe16ba37cf7ed8986588d5
Silvan Spycher <silvan.spycher@gmail.com>
2017-05-08 23:12:30 +0200

    added levelbuilder that actually works. but without parser

--- Breakout/Assets/Scripts/LevelBuilder.cs
+++ Breakout/Assets/Scripts/LevelBuilder.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBuilder : MonoBehaviour {
+public class LevelBuilder : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private GameObject block;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    public bool BuildLevel(int levelnumber)
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            for (int j = -10; j < 10; j += 2)
+            {
+                GameObject newblock = Instantiate(block) as GameObject;
+                newblock.transform.position = new Vector3(j, i, 0);
+
+            }
+        }
+
+
+        return true;
+    }
+
+
+
+
+
 }
